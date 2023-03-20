f405bbeff1f1295027b5c6bb680a34d0408057dd
kkr9551 <71511316+kkr9551@users.noreply.github.com>
2023-03-20 10:28:39 +0200

    delete files and function around change pw

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,10 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
-import bcrypt from 'bcrypt';
+/*import bcrypt from 'bcrypt';
 import Token from "../models/tokenModel.js";
 import crypto from "crypto";
-import sendEmail from "../utils/sendemail.js";
+import sendEmail from "../utils/sendemail.js";*/
 
 /**get user data */
 export const getUser = asyncHandler(async (req, res) => {
@@ -62,25 +62,25 @@ export const updateUser = asyncHandler(async (req, res) => {
 });
 
 /**change password */
-export const changePassword = asyncHandler(async (req, res) => {
+/*export const changePassword = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
 
     const {oldPassword, password} = req.body;
     if (!user) {
         res.status(400);
         throw new Error("User not found, please sign up first");
-    }
+    }*/
     //validate
-    if (!oldPassword || !password) {
+    /*if (!oldPassword || !password) {
         res.status(400);
         throw new Error("Please fill in both old and new passwords");
-    }
+    }*/
 
     //check if old password matches password in the database
-    const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+    /*const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);*/
 
     //save new password
-    if (user && passwordIsCorrect) {
+    /*if (user && passwordIsCorrect) {
         user.password = password;
         await user.save();
         res.status(200).send("Password change successfully");
@@ -88,39 +88,39 @@ export const changePassword = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("Old password is incorrect");
     }
-});
+});*/
 
 /**forgot password */
-export const forgotPassword = asyncHandler(async (req, res) => {
+/*export const forgotPassword = asyncHandler(async (req, res) => {
     const {email} = req.body;
     const user = await User.findOne({email});
 
     if (!user) {
         res.status(404)
         throw new Error("User does not exist");
-    }
+    }*/
 
     //create reset token
-    let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
+    //let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
     //console.log(resetToken);
     
     //hash token before saving to DB
-    const hashedToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+    //const hashedToken = crypto.createHash("sha256").update(resetToken).digest("hex");
     //console.log(hashedToken);
 
     //save token to DB
-    await new Token({
+    /*await new Token({
         userId: user._id,
         token: hashedToken,
         createdAt: Date.now(),
         expiresAt: Date.now() + 30 * (60 * 1000),
-    }).save();
+    }).save();*/
 
     //construct reset url
-    const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
+    //const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
     
     //reset email
-    const message = `
+    /*const message = `
         <h2>Hello ${user.name}</h2>
         <p>Please use the url below to reset your password</p>
         <p>This reset link is valid for only 30 minutes</p>
@@ -142,7 +142,7 @@ export const forgotPassword = asyncHandler(async (req, res) => {
         res.status(500);
         throw new Error("Email not sent, please try again");
     }
-});
+});*/
 
 /*export const getUserWitnesses = async (req, res) => {
     try {
